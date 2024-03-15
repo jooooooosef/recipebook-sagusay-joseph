ab33a00c117236dfea873d854c6a02da5159e62a
Joseph Sagusay <joseph.sagusay@student.ateneo.edu>
2024-03-15 19:13:08 +0800

    Restricted Bio field. Must be at least 255 characters.

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    bio = models.TextField()
+    bio = models.TextField(
+        validators=[MinLengthValidator(255, "Bio should be at least 255 characters.")]
+    )
 
     def __str__(self):
         return self.name
